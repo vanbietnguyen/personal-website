61224aa66a9cebfcc7ab1f16d3c4428729a93948
vanbietnguyen <vanbietnguyen@gmail.com>
2021-11-05 14:19:19 -0400

    adding helmet

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Profile from './components/Profile'
 import AboutMe from './components/AboutMe'
 import Skills from './components/Skills'
 import Salutations from './components/Salutations'
-
+import {Helmet} from "react-helmet";
 
 function App() {
 
   return (
     <div>
+      <Helmet>
+        <title>vanbiet nguyen</title>
+        <meta name="description" content="my website" />
+    </Helmet>
       <Header />
         <main>
           <Profile />
@@ -26,7 +30,3 @@ function App() {
 
 export default App;
 
-// <Routes>
-//             <Route exact path='/' element={<Profile />}/>
-//             <Route path='/about' element={<AboutMe />}/>
-//           </Routes>
