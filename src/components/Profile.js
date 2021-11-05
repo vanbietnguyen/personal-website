4a48855fac15ac814b3f10e1a46f5d35cf54087d
vanbietnguyen <vanbietnguyen@gmail.com>
2021-11-05 14:03:07 -0400

    final mediaquery changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,18 +8,26 @@ const Profile = () => {
     return (
             <div id="home" className="centered">
                 <h1 id="title-text">
-                    <span className="anim2">So</span>
-                    <span className="anim1">ft</span>
-                    <span className="anim4">wa</span>
-                    <span className="anim3">re</span>
+                    <span className="inverted">s</span>
+                    <span className="anim1 inverted">o</span>
+                    <span className=" inverted">f</span>
+                    <span className="anim2 inverted">t</span>
+                    <span className="inverted">w</span>
+                    <span className="anim1 inverted">a</span>
+                    <span className="inverted">r</span>
+                    <span className="inverted">e</span>
                     <br/>
-                    <span className="anim2">En</span>
-                    <span className="anim1">gi</span>
-                    <span className="anim4">ne</span>
-                    <span className="anim3">er</span>
-                    <span className="anim4"></span>
+                    <span className="anim2">E</span>
+                    <span className="">n</span>
+                    <span className="anim1" >g</span>
+                    <span className="" >i</span>
+                    <span className="anim2">n</span>
+                    <span className="">e</span>
+                    <span className="">e</span>
+                    <span className="anim1">r</span>
+                    <span className=" inverted"></span>
                 </h1>
-                <img id='prof-pic' alt='me' src="https://github.com/vanbietnguyen/personal-website/blob/main/.github/me.jpg?raw=true" />
+                <img id='prof-pic' alt='me' src="https://lh3.googleusercontent.com/5r4rpc1QumymJrUBREG9Ln2M-NCJ5KX7uOXMl1vj-fjvHZf_Nbihel4IbHjWI_CK2vErT40tpsh5UDkAciQ2nyO8jCEamAH3EeV6EYPcokxH2ifiiXCo8AzY21X4MzUKYbha_HIuUwWgHVfafEJLChHgsnbjYL_hNiHSgxr8-YNniL14haF7CHBoWUdkt_RY5_bAlCwWsyplNJs7qCwkNNPCVLzzPKolnl1BSzvm4siOss_tiSCyF1bLLI-D6MLIX-AhE2L1if1Cp1n9oyz6yg9yGCqJrh346m8DOuREwWpZkVPin0euRPZ7AIeVJdjJkyaZEm-YCDgKC9-KYumxHlMQsEUMFVzByS4Xi-zd-MVj8oUYrDFQoyx_CWpKNUYRxND1o1MSCznjaxFSIzu5Sf2QjxHmKpfsjnW2mytrhY6CC7SrDaSKwoKrD0mYuBVEHRtO_rAJhAX3tUza5fD1YO0SyB86D00KXXCtsjZta_g6fp64nGqxJB-TmdoaIJmejtdKQOzY2lyhag0Pdss8hpVaIvVJpXkQuYuJNn_3Lp7qQzFy8XNIV9ZlXa_QueOrD9zYxIVifsiDqloYX9cCQDwHgLyx-4eiBBhyVQdEkW1LZuGm8QavdBSaGeKz3yOVrJgVgSmvQgCLvx-AHXwjOSX7UpqQ7ejTchQEbQsQBAIF6f3NnfFcveP5W7AA-sHlymF_dlAfLbuvdSYyoYQ=s804-no?authuser=0" />
             </div>
     )
 }
